cc72b0fe19fd6ff2a6fe4345f8efd7dea73e4120
chichilu-01 <hoangminhphuong270401@gmail.com>
2025-12-05 06:24:26 +0000

    HG

--- utils/sendResetCodeEmail.js
+++ utils/sendResetCodeEmail.js
@@ -1,12 +1,24 @@
-import transporter from "./mailClient.js";
+// 📁 backend/utils/sendResetCodeEmail.js
+
+import sgMail from "@sendgrid/mail";
+
+// Thiết lập API Key (sử dụng biến môi trường SENDGRID_API_KEY)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendResetCodeEmail = async (to, code) => {
-  await transporter.sendMail({
-    from: `CHICHILU Shop <${process.env.EMAIL_USER}>`,
-    to,
+  // SENDER phải là email đã được Verified: phoenix**om274@gmail.com
+  const senderEmail = process.env.EMAIL_USER;
+
+  const msg = {
+    to: to,
+    // Dùng tên đã được Verified
+    from: `CHICHILU Shop <${senderEmail}>`,
     subject: "🔐 Mã xác nhận đặt lại mật khẩu",
     html: `<p>Mã đặt lại mật khẩu của bạn là: <b>${code}</b></p><p>Có hiệu lực trong 5 phút.</p>`,
-  });
+  };
+
+  // Gửi qua API (HTTPS)
+  await sgMail.send(msg);
 };
 
 export default sendResetCodeEmail;
--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -1,16 +1,25 @@
-import transporter from "./mailClient.js";
+// 📁 backend/utils/sendVerificationEmail.js
+
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendVerificationEmail = async (to, code) => {
-  await transporter.sendMail({
-    from: `Shop Replit <${process.env.EMAIL_USER}>`,
-    to,
+  const senderEmail = process.env.EMAIL_USER;
+
+  const msg = {
+    to: to,
+    from: `Shop Replit <${senderEmail}>`,
     subject: "🔐 Mã xác nhận đăng ký",
     html: `
-      <p>Xin chào,</p>
-      <p>Mã xác nhận đăng ký của bạn là: <b>${code}</b></p>
-      <p>Mã có hiệu lực trong 5 phút.</p>
-    `,
-  });
+            <p>Xin chào,</p>
+            <p>Mã xác nhận đăng ký của bạn là: <b>${code}</b></p>
+            <p>Mã có hiệu lực trong 5 phút.</p>
+        `,
+  };
+
+  // Gửi qua API (HTTPS)
+  await sgMail.send(msg);
 };
 
 export default sendVerificationEmail;
